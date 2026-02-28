65a6befebd8188ef013d9522b6012ec2caffa095
Arslan Ahmad <mr.arsal69@gmail.com>
2026-02-28 14:11:27 +0500

    Updates

---
+++ _next/static/2NLe_lYP0xwFNcS1T7ZhU/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/clothing-brand-nexts-prototype//_next/:path+",
+        "destination": "/clothing-brand-nexts-prototype/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
