84de8784e05a4c0af80759bffde4eb16236e68db
Arslan Ahmad <mr.arsal69@gmail.com>
2026-03-01 12:35:03 +0500

    vercel-config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  basePath: "/clothing-brand-nexts-prototype",
-  assetPrefix: "/clothing-brand-nexts-prototype/",
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
